9f34e2ef7ed9faab21729078ad10665e6424d30a
Samuel Hammersberg <samuel@loafey.se>
2025-03-10 23:38:25 +0100

    Kinda done.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use anyhow::Result;
-use egui::{CollapsingHeader, Color32, RichText, ScrollArea};
+use audiotags::Tag;
+use egui::{CollapsingHeader, Color32, RichText, ScrollArea, Ui};
+use rayon::prelude::*;
 use std::{
     collections::{BTreeMap, BTreeSet},
     fs,
+    sync::{
+        atomic::{AtomicUsize, Ordering::Relaxed},
+        mpsc::{channel, Receiver, Sender},
+    },
+    thread,
+    time::Duration,
 };
 
 const ROOT: &str = "/home/loafey/BreadBox/Jellyfin/Music/Lidarr";
@@ -12,6 +20,8 @@ type Albums = BTreeMap<String, Album>;
 type Artist = String;
 type Album = BTreeSet<Song>;
 type Song = String;
+
+const MISSING: &str = "-- MISSING TITLE --";
 
 fn is_song(end: &str) -> bool {
     end.ends_with(".3gp")
@@ -58,9 +68,10 @@ fn is_song(end: &str) -> bool {
         || end.ends_with(".cda")
 }
 
-fn get_data() -> Result<Artists> {
+fn get_data(sender: &mut Sender<Message>) -> Result<Artists> {
     let artists = fs::read_dir(ROOT)?;
     let mut top = Artists::new();
+    let mut total_songs = 0;
     for artist in artists {
         let mut albums_data = Albums::new();
         let artist = artist?;
@@ -83,15 +94,44 @@ fn get_data() -> Result<Artists> {
                 }
                 let song_name = song.file_name().to_string_lossy().to_string();
                 if is_song(&song_name) {
-                    album_data.insert(song_name);
+                    // let song_name = Tag::new()
+                    //     .read_from_path(song.path())?
+                    //     .title()
+                    //     .unwrap_or(MISSING)
+                    //     .to_string();
+                    album_data.insert(song.path().to_string_lossy().to_string());
+                    total_songs += 1;
                 }
             }
 
             albums_data.insert(album_name, album_data);
         }
 
+        sender.send(Message::Loading(0, total_songs)).unwrap();
+
         top.insert(artist_name, albums_data);
     }
+
+    let fixed = AtomicUsize::new(0);
+    for albums in top.values_mut() {
+        albums.values_mut().par_bridge().for_each(|songs| {
+            let mut new_songs = BTreeSet::new();
+            for song in songs.clone() {
+                let song_name = Tag::new()
+                    .read_from_path(song)
+                    .unwrap()
+                    .title()
+                    .unwrap_or(MISSING)
+                    .to_string();
+                new_songs.insert(song_name);
+                let s = fixed.fetch_add(1, Relaxed);
+                sender.send(Message::Loading(s, total_songs)).unwrap();
+            }
+
+            *songs = new_songs;
+        });
+    }
+
     Ok(top)
 }
 
@@ -100,11 +140,19 @@ enum Info {
     PartialSubset(String, String, Vec<String>),
     Subset(String, String),
     Empty,
+    MissingTitle,
 }
 type InfoTree = BTreeMap<Artist, BTreeMap<String, Vec<Info>>>;
-fn get_info(artists: &Artists) -> InfoTree {
+fn get_info(sender: &mut Sender<Message>, artists: &Artists) -> InfoTree {
     let mut top = InfoTree::new();
 
+    let mut total_albums = 0;
+    for albums in artists.values() {
+        total_albums += albums.len();
+        sender.send(Message::Loading(0, total_albums)).unwrap();
+    }
+
+    let mut current_album = 0;
     for (artist, albums) in artists {
         let mut artist_info = BTreeMap::new();
 
@@ -116,6 +164,18 @@ fn get_info(artists: &Artists) -> InfoTree {
             if songs_a.is_empty() {
                 album_info.push(Info::Empty);
                 is_empty = true;
+            }
+
+            // Find missing names
+            let mut missing = false;
+            for song in songs_a {
+                if song == MISSING {
+                    missing = true;
+                    break;
+                }
+            }
+            if missing {
+                album_info.push(Info::MissingTitle);
             }
 
             // find subsets
@@ -149,6 +209,11 @@ fn get_info(artists: &Artists) -> InfoTree {
             if !album_info.is_empty() {
                 artist_info.insert(album_a.clone(), album_info);
             }
+
+            current_album += 1;
+            sender
+                .send(Message::Loading(current_album, total_albums))
+                .unwrap();
         }
 
         if !artist_info.is_empty() {
@@ -160,87 +225,154 @@ fn get_info(artists: &Artists) -> InfoTree {
 }
 
 fn main() -> Result<()> {
-    let artists = get_data()?;
-    let info = get_info(&artists);
+    let (mut sender, reciever) = channel();
+    thread::spawn(move || {
+        let artists = get_data(&mut sender).unwrap();
+        let info = get_info(&mut sender, &artists);
+        sender.send(Message::Done(artists, info)).unwrap();
+    });
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "subset-album",
         native_options,
-        Box::new(|_| Ok(Box::new(App { artists, info }))),
+        Box::new(|_| {
+            Ok(Box::new(App {
+                artists: Default::default(),
+                info: Default::default(),
+                reciever,
+                loading_status: Some((0, usize::MAX)),
+            }))
+        }),
     )
     .unwrap();
     Ok(())
 }
 
+enum Message {
+    Loading(usize, usize),
+    Done(Artists, InfoTree),
+}
+
 struct App {
+    loading_status: Option<(usize, usize)>,
+    reciever: Receiver<Message>,
     artists: Artists,
     info: InfoTree,
 }
-impl eframe::App for App {
-    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.columns(2, |ui| {
-                ScrollArea::vertical()
-                    .auto_shrink([false, false])
-                    .id_salt("all-albums")
-                    .show(&mut ui[0], |ui| {
-                        for (artist, albums) in &self.artists {
-                            CollapsingHeader::new(artist)
-                                .id_salt(format!("{artist}-info"))
-                                .show(ui, |ui| {
-                                    for (album, songs) in albums {
-                                        ui.collapsing(album, |ui| {
-                                            for song in songs {
-                                                ui.label(song);
-                                            }
-                                        });
-                                    }
-                                });
-                        }
-                    });
-                ScrollArea::vertical()
-                    .auto_shrink([false, false])
-                    .id_salt("overlapps")
-                    .show(&mut ui[1], |ui| {
-                        for (artist, tree) in &self.info {
-                            ui.collapsing(artist, |ui| {
-                                for (album, fields) in tree {
-                                    CollapsingHeader::new(album).default_open(true).show(
-                                        ui,
-                                        |ui| {
-                                            for field in fields {
-                                                let (text, color, bread) = match &field {
-                                                    Info::PartialSubset(a, b, songs) => (
-                                                        "Partial subset",
-                                                        Color32::YELLOW,
-                                                        format!(
-                                                            "{a:?} is a partial subset of {b:?}\n\n{}",songs.iter().map(|s|format!("- {s}")).collect::<Vec<_>>().join(",")
-                                                        ),
-                                                    ),
-                                                    Info::Subset(a, b) => (
-                                                        "Subset",
-                                                        Color32::GREEN,
-                                                        format!("{a:?} is a subset of {b:?}"),
-                                                    ),
-                                                    Info::Empty => (
-                                                        "Empty",
-                                                        Color32::RED,
-                                                        "this album contains no songs".to_string(),
-                                                    ),
-                                                };
-                                                let label = RichText::new(text).color(color);
-                                                ui.horizontal_wrapped(|ui| {
-                                                    ui.label(label);
-                                                    ui.label(bread);
-                                                });
-                                            }
-                                        },
-                                    );
+impl App {
+    fn draw_data(&self, ui: &mut Ui) {
+        ui.columns(2, |ui| {
+            ScrollArea::vertical()
+                .auto_shrink([false, false])
+                .id_salt("all-albums")
+                .show(&mut ui[0], |ui| {
+                    for (artist, albums) in &self.artists {
+                        CollapsingHeader::new(artist)
+                            .id_salt(format!("{artist}-info"))
+                            .show(ui, |ui| {
+                                for (album, songs) in albums {
+                                    ui.collapsing(album, |ui| {
+                                        for song in songs {
+                                            ui.label(song);
+                                        }
+                                    });
                                 }
                             });
-                        }
-                    })
-            });
+                    }
+                });
+            ScrollArea::vertical()
+                .auto_shrink([false, false])
+                .id_salt("overlapps")
+                .show(&mut ui[1], |ui| {
+                    for (artist, tree) in &self.info {
+                        ui.collapsing(artist, |ui| {
+                            for (album, fields) in tree {
+                                CollapsingHeader::new(album)
+                                    .default_open(true)
+                                    .show(ui, |ui| {
+                                        for field in fields {
+                                            let (text, color, bread) = match &field {
+                                                Info::PartialSubset(a, b, songs) => (
+                                                    "Partial subset",
+                                                    Color32::YELLOW,
+                                                    format!(
+                                                        "{a:?} is a partial subset of {b:?}\n\n{}",
+                                                        songs
+                                                            .iter()
+                                                            .map(|s| format!("- {s}"))
+                                                            .collect::<Vec<_>>()
+                                                            .join(",")
+                                                    ),
+                                                ),
+                                                Info::MissingTitle => {
+                                                    ("Missing titles", Color32::BLUE, String::new())
+                                                }
+                                                Info::Subset(a, b) => (
+                                                    "Subset",
+                                                    Color32::GREEN,
+                                                    format!("{a:?} is a subset of {b:?}"),
+                                                ),
+                                                Info::Empty => (
+                                                    "Empty",
+                                                    Color32::RED,
+                                                    "this album contains no songs".to_string(),
+                                                ),
+                                            };
+                                            let label = RichText::new(text).color(color);
+                                            ui.horizontal_wrapped(|ui| {
+                                                ui.label(label);
+                                                ui.label(bread);
+                                            });
+                                        }
+                                    });
+                            }
+                        });
+                    }
+                });
         });
+    }
+}
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, _: &mut eframe::Frame) {
+        if let Ok(m) = self.reciever.try_recv() {
+            match m {
+                Message::Loading(a, b) => self.loading_status = Some((a, b)),
+                Message::Done(artists, info) => {
+                    self.loading_status = None;
+                    self.artists = artists;
+                    self.info = info;
+                }
+            }
+        }
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if let Some((cur, max)) = self.loading_status {
+                ui.centered_and_justified(|ui| {
+                    let p = if max != 0 && cur != 0 {
+                        let progress = cur as f32 / max as f32;
+                        let progress_bar_len = 20;
+                        format!(
+                            "\n{}",
+                            (0..progress_bar_len)
+                                .map(|i| {
+                                    let percent = i as f32 / progress_bar_len as f32;
+                                    if percent < progress {
+                                        '◼'
+                                    } else {
+                                        '◻'
+                                    }
+                                })
+                                .collect::<String>()
+                        )
+                    } else {
+                        String::new()
+                    };
+                    ui.heading(format!("{cur}/{max}{p}"));
+                });
+            } else {
+                self.draw_data(ui);
+            }
+        });
+        ctx.request_repaint_after(Duration::from_secs_f64(0.066));
     }
 }
